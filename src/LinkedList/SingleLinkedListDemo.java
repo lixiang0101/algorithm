e63b0931a505a8fe0412955cbb726a340958187f
lixiang0101 <451711816@qq.com>
2020-02-05 14:59:06 +0800

    algorithm 4 commit

--- src/LinkedList/SingleLinkedListDemo.java
+++ src/LinkedList/SingleLinkedListDemo.java
@@ -9,9 +9,24 @@ public class SingleLinkedListDemo{
         singleLinkedList.addNode(node2);
         //singleLinkedList.showNode();
 
+        // 测试按顺序添加
         HeroNode node3 = new HeroNode(1,"卢俊义","玉麒麟");
         singleLinkedList.addNodeByOrder(node3);
+        //singleLinkedList.showNode();
+
+        // 测试修改node
+        HeroNode node4 = new HeroNode(1,"卢俊义1","玉麒麟");
+        singleLinkedList.update(node4);
         singleLinkedList.showNode();
+
+        // 测试删除节点
+        System.out.println("测试删除节点");
+        singleLinkedList.delete(4);
+        singleLinkedList.showNode();
+        System.out.println("链表的长度为："+singleLinkedList.getLength(singleLinkedList.getHead()));
+
+        System.out.println("获取倒数第1个节点");
+        System.out.println(SingleLinkedList.findLastIndexNode(singleLinkedList.getHead(), 2).toString());
     }
 }
 
@@ -19,6 +34,10 @@ public class SingleLinkedListDemo{
 class SingleLinkedList { // 定义单链表类
     // 创建头节点，头节点指向单链表的第一个节点
     private HeroNode head = new HeroNode(0,"","");
+
+    public HeroNode getHead() {
+        return head;
+    }
 
     // 添加到新节点到单链表上
     public void addNode(HeroNode node){
@@ -70,6 +89,85 @@ class SingleLinkedList { // 定义单链表类
             tmp.next = node;
         }
     }
+    // 更新节点数据
+    public void update(HeroNode node){
+        if (head.next == null){
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode tmp = head;
+        while (true){
+            if (tmp.next == null){ //链表遍历结束
+
+            }
+            if (tmp.next.no == node.no ){
+                node.next = tmp.next.next;
+                tmp.next = node;
+                break;
+            }
+            tmp = tmp.next;
+        }
+    }
+    //删除节点
+    //1、先找到要删除的节点的前一个节点 tmp
+    //2、让tmp指向tmp.next.next
+    //3、被删除的节点没有引用指向它，就会被垃圾回收机制回收
+    public void delete(int no){
+        if (head.next == null){
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode tmp = head;
+        boolean flag = false;
+        while (true){
+            if (tmp.next == null){ //如果没有找到要删除的节点
+                System.out.println("链表遍历结束");
+                break;
+            }else if(tmp.next.no == no){
+                flag = true;
+                break;
+            }
+            tmp = tmp.next;
+        }
+        if (flag){ // 找到了要删除的节点
+            tmp.next = tmp.next.next;
+        }else {
+            System.out.println("没有找到要删除的节点");
+        }
+    }
+    // 获取单链表长度，不计算头节点
+    // 首先判断链表是不是为空
+    // 然后用临时变量往下遍历，如果tmp != null，长度加1
+    public static int getLength(HeroNode head){
+        if (head.next == null){
+            return 0;
+        }
+        int length = 0;
+        HeroNode tmp = head.next;
+        while (tmp != null){
+            length ++;
+            tmp = tmp.next;
+        }
+        return length;
+    }
+
+    //查找单链表中的倒数第k个节点【新浪面试题】
+    public static HeroNode findLastIndexNode(HeroNode headNode,int index){
+        if (headNode.next == null ){
+            //throw new RuntimeException("链表为空");
+            return null;
+        }
+        int length = SingleLinkedList.getLength(headNode);
+        HeroNode tmp = headNode;
+        if (index > length){
+            throw new RuntimeException("超过链表范围");
+        }
+        for (int i = 0; i < (length - index ); i++) {
+            tmp = tmp.next;
+        }
+        return tmp.next;
+    }
+
 }
 
 
